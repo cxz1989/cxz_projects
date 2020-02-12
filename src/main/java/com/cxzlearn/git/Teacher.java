dcebe332ddd56e1c492c54955701011089052976
chenxz <1412939008@qq.com>
2020-02-12 16:22:27 +0800

    cxz_projects develop add Teacher.java类

---
+++ src/main/java/com/cxzlearn/git/Teacher.java
@@ -0,0 +1,42 @@
+package com.cxzlearn.git;
+
+public class Teacher {
+
+	private String uername;
+	private String password;
+	private String address;
+	private int age;
+
+	public String getUername() {
+		return uername;
+	}
+
+	public void setUername(String uername) {
+		this.uername = uername;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
